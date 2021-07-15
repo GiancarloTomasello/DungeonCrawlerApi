2b0d8710fcd54d2854c499b70a8ab9c5f3ab322c
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-07-15 12:19:07 -0700

    dependencies and main application file

---
+++ src/main/java/com/giancarlo/dungeonCrawler/DungeonCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.giancarlo.dungeonCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungeonCrawlerApplication.class, args);
+    }
+}
